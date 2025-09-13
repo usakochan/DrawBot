e43092b821f0ae64aa1253ea0d21f90afe335f83
usakochan <ryokun8500@keio.jp>
2025-09-14 01:53:00 +0900

    drawing smiling face like picaso

--- first try.py
+++ first try.py
@@ -4,3 +4,25 @@ print("w:", width())
 print("h:", height())
 
 rect(200, 100, 100, 300)
+oval(50, 350, 100, 100)
+polygon((375, 475), (400, 450), (400, 350), (350, 400), close=False)
+
+# create path
+newPath()
+
+moveTo((350, 20))
+lineTo((380, 50))
+curveTo((400, 80), (450, 150), (400, 230))
+lineTo((330, 180))
+
+# close the path
+closePath()
+# draw the path
+drawPath()
+
+
+
+newPage(900, 900)
+
+font('Inter', 100)
+text('He is smiling!', (000, 50))
